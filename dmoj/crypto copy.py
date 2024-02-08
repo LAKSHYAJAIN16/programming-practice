606413794cc6521edd2b4c820142339a9737b407
Lakshya Jain <minecraftpogtechno@gmail.com>
2024-02-08 12:51:22 -0500

    kill yourself

---
+++ dmoj/crypto copy.py
@@ -0,0 +1,7 @@
+string = input()
+l = []
+p = {}
+
+for k in range(len(string), 2):
+    l.append(string[k])
+    l.append(string[k + 1])
---
+++ dmoj/simulation copy.py
@@ -0,0 +1,7 @@
+max_found = 0
+search_idx = 0
+s = input()
+
+for l in s:
+    if l == "d":
+        search_idx = 1
